7eda3b6037bca13e67d7ab755830b4cfd92479a8
LeThanh-Phat <lethanhphat09849@gmail.com>
2025-04-25 20:15:46 +0700

    buoihoc

--- app/Models/BuoiHoc.php
+++ app/Models/BuoiHoc.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Buoihoc extends Model
+class BuoiHoc extends Model
 {
-    use HasFactory;
+    protected $table = 'buoihoc';
+
+    protected $primaryKey = 'id_buoihoc';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ngayhoc',
+        'tietbd',
+        'tietkt',
+        'phonghoc',
+        'id_lophoc',
+        'qr_codedata',
+        'thoigianhethan',
+        'id_admin',
+    ];
+
+    // Quan hệ với model DiemDanh
+    public function diemDanhs()
+    {
+        return $this->hasMany(DiemDanh::class, 'id_buoihoc', 'id_buoihoc');
+    }
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class, 'id_admin');
+    }
+    // Quan hệ với model LopHoc (nếu có)
+    public function lopHoc()
+    {
+        return $this->belongsTo(LopHoc::class, 'id_lophoc', 'id_lophoc');
+    }
 }
